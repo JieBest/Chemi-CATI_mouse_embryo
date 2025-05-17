c0e66a7724da4ca0bf151bc9f1fd7deef2d4b2a0
JieBest <89961915+JieBest@users.noreply.github.com>
2025-05-17 19:38:35 +0800

    Update preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -1,1 +1,7 @@
 
+for sample in `cat allsample.txt`; 
+do 
+  echo $sample
+  trim_galore -o $trimout -j 4 --gzip --paired ${inputdir}${sample}_combined_R1.fastq.gz ${inputdir}${sample}_combined_R2.fastq.gz
+  rsem-calculate-expression --no-bam-output --paired-end --alignments -p 10 ${rsemout}/${sample}_Aligned.toTranscriptome.out.bam $ref ${rsemout}/${sample}.rsem -q 
+done
